3e6782bdf2f35ae05e5c647b8ebcebeb4e3255be
Nouman Naeem <naeemnoman548@gmail.com>
2024-05-09 15:28:22 +0500

    home-layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
-import {Home} from "./home/home.tsx";
+import {Home} from "./home/layout/home.tsx";
 import {Authentication} from "./authentication/layout/authentication.tsx";
 import {SignIn} from "./authentication/pages/signIn.tsx";
 import {SignUp} from "./authentication/pages/signUp.tsx";
