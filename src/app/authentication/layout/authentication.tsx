f56bb9eb1b113818ba230307dae35999db267e7b
Nouman Naeem <naeemnoman548@gmail.com>
2024-05-18 00:47:16 +0500

    authentication page

--- src/app/authentication/layout/authentication.tsx
+++ src/app/authentication/layout/authentication.tsx
@@ -1,33 +1,47 @@
-import Box from "@mui/material/Box";
-import vfs_logo from '../../../assets/VFS_logo.png';
+import vfs_logo_dark from '../../../assets/VFS_logo_dark.png';
+import vfs_logo_light from '../../../assets/VFS_logo_light.png';
 import Typography from "@mui/material/Typography";
 import {Outlet} from "react-router-dom";
+import {useContext} from "react";
+import {ThemeContext} from "../../core/providers/themeProvider.tsx";
+import {Card, ThemeProvider} from "@mui/material";
+import Box from "@mui/material/Box";
 
 export function Authentication() {
+    const {theme} = useContext(ThemeContext);
+    let logo = theme.palette.mode === 'dark' ? vfs_logo_light : vfs_logo_dark;
     return (
         <>
-            <Box sx={{height: 'inherit', background: 'var(--gm3-sys-color-surface-container, #f0f4f9)'}}>
-                <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'inherit'}}>
-                    <Box sx={{backgroundColor: 'white', width: '900px', borderRadius: '30px'}}>
-                        <Box sx={{m: '35px'}}>
-                            <img src={vfs_logo} alt="logo" height="50px"/>
-                            <Box sx={{mt: '20px', display: 'flex', justifyContent: 'space-between'}}>
-                                <Box>
-                                    <Typography sx={{fontSize: '40px', mb: '10px'}}>
-                                        Sign in
-                                    </Typography>
-                                    <Typography>
-                                        Use your TMS account
-                                    </Typography>
-                                </Box>
-                                <Box>
-                                    <Outlet/>
+            <ThemeProvider theme={theme}>
+                <Box sx={{height: 'inherit'}}>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: 'inherit',
+                        background: theme.palette.mode === 'light' ? 'var(--gm3-sys-color-surface-container, #f0f4f9)' : '#000000eb'
+                    }}>
+                        <Card variant='outlined' sx={{width: '900px', borderRadius: '30px'}}>
+                            <Box sx={{m: '35px'}}>
+                                <img src={logo} alt="logo" height="50px"/>
+                                <Box sx={{mt: '20px', display: 'flex', justifyContent: 'space-between'}}>
+                                    <Box>
+                                        <Typography sx={{fontSize: '40px', mb: '10px'}}>
+                                            Sign in
+                                        </Typography>
+                                        <Typography>
+                                            Use your TMS account
+                                        </Typography>
+                                    </Box>
+                                    <Box>
+                                        <Outlet/>
+                                    </Box>
                                 </Box>
                             </Box>
-                        </Box>
+                        </Card>
                     </Box>
                 </Box>
-            </Box>
+            </ThemeProvider>
         </>
     )
 }
