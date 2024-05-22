e6c0f8db39177e5f0df2226816d1ee173eca294f
Nouman Naeem <naeemnoman548@gmail.com>
2024-05-22 21:55:51 +0500

    add user dialog removed

--- src/app/home/pages/users/users.tsx
+++ src/app/home/pages/users/users.tsx
@@ -6,7 +6,6 @@ import {UsersTable} from "./components/usersTable.tsx";
 import {Box} from "@mui/material";
 import {LoaderContext} from "../../../core/providers/loaderProvider.tsx";
 import Button from "@mui/material/Button";
-import {AddUserDialog} from "./components/addUserDialog.tsx";
 
 export function Users() {
     const {toggleLoading} = useContext(LoaderContext);
@@ -24,38 +23,8 @@ export function Users() {
     return (
         <>
             <Box sx={{textAlign: "end"}}>
-                <Box sx={{m: '15px 0'}}>
-                    <AddUserDialogButton addNewUser={addNewUser}/>
-                </Box>
                 <UsersTable users={users}/>
             </Box>
         </>
     )
-}
-
-
-function AddUserDialogButton({addNewUser}: { addNewUser: (data: User) => void }) {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = (data?: User) => {
-        setOpen(false);
-        if (data)
-            addNewUser(data);
-    };
-
-    return (
-        <>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Add User
-            </Button>
-            <AddUserDialog
-                open={open}
-                onClose={handleClose}
-            />
-        </>
-    );
 }
